fba5fcec2a8025745bb363fd9494e61bdff95924
Xinyu Wen <helloxinyuwen@gmail.com>
2025-03-24 15:15:29 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import { retrieveStockData } from './main';
-import { testJSON } from './shared/util';
 
 function main() {
   retrieveStockData().subscribe();
 }
 
 main();
-
-function test() {
-  testJSON();
-}
-
-// test();
